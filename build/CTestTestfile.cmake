47cf5207f20045b301f31a0251a9a2a43f02ee34
David02345 <02william02@hanmail.net>
2025-05-06 16:52:45 +0900

    serial comm test

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ydg/serial
+# Build directory: /home/ydg/serial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/ydg/serial/tests
+# Build directory: /home/ydg/serial/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(serial-test "/usr/bin/python3" "-u" "/opt/ros/humble/share/ament_cmake_test/cmake/run_test.py" "/home/ydg/serial/build/test_results/serial/serial-test.gtest.xml" "--package-name" "serial" "--output-file" "/home/ydg/serial/build/ament_cmake_gtest/serial-test.txt" "--command" "/home/ydg/serial/build/tests/serial-test" "--gtest_output=xml:/home/ydg/serial/build/test_results/serial/serial-test.gtest.xml")
+set_tests_properties(serial-test PROPERTIES  LABELS "gtest" REQUIRED_FILES "/home/ydg/serial/build/tests/serial-test" TIMEOUT "60" WORKING_DIRECTORY "/home/ydg/serial/build/tests" _BACKTRACE_TRIPLES "/opt/ros/humble/share/ament_cmake_test/cmake/ament_add_test.cmake;125;add_test;/opt/ros/humble/share/ament_cmake_gtest/cmake/ament_add_gtest_test.cmake;86;ament_add_test;/opt/ros/humble/share/ament_cmake_gtest/cmake/ament_add_gtest.cmake;93;ament_add_gtest_test;/home/ydg/serial/tests/CMakeLists.txt;5;ament_add_gtest;/home/ydg/serial/tests/CMakeLists.txt;0;")
+add_test(serial-test-timer "/usr/bin/python3" "-u" "/opt/ros/humble/share/ament_cmake_test/cmake/run_test.py" "/home/ydg/serial/build/test_results/serial/serial-test-timer.gtest.xml" "--package-name" "serial" "--output-file" "/home/ydg/serial/build/ament_cmake_gtest/serial-test-timer.txt" "--command" "/home/ydg/serial/build/tests/serial-test-timer" "--gtest_output=xml:/home/ydg/serial/build/test_results/serial/serial-test-timer.gtest.xml")
+set_tests_properties(serial-test-timer PROPERTIES  LABELS "gtest" REQUIRED_FILES "/home/ydg/serial/build/tests/serial-test-timer" TIMEOUT "60" WORKING_DIRECTORY "/home/ydg/serial/build/tests" _BACKTRACE_TRIPLES "/opt/ros/humble/share/ament_cmake_test/cmake/ament_add_test.cmake;125;add_test;/opt/ros/humble/share/ament_cmake_gtest/cmake/ament_add_gtest_test.cmake;86;ament_add_test;/opt/ros/humble/share/ament_cmake_gtest/cmake/ament_add_gtest.cmake;93;ament_add_gtest_test;/home/ydg/serial/tests/CMakeLists.txt;15;ament_add_gtest;/home/ydg/serial/tests/CMakeLists.txt;0;")
+subdirs("../gtest")
